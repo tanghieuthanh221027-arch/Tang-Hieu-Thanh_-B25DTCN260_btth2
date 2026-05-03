07d6d1caa1c69c9cb9096150a32966e712cea6a4
tanghieuthanh221027-arch <tanghieuthanh221027@gmail.com>
2026-05-03 22:25:58 +0700

    Add multiple SQL queries for student and course data

--- btth2.sql
+++ btth2.sql
@@ -167,3 +167,40 @@ WHERE
     e.score IS NOT NULL
 GROUP BY c.id , c.course_name
 HAVING AVG(e.score) < 5.0;
+
+SELECT 
+    *
+FROM
+    students
+WHERE
+    date_of_birth > (SELECT 
+            date_of_birth
+        FROM
+            students
+        WHERE
+            id = 1);
+
+SELECT s.id,s.full_name,e.score
+FROM enrollments e
+JOIN students s ON e.student_id = s.id
+JOIN courses c ON e.course_id = c.id
+WHERE c.course_name = 'Tiếng Anh Giao Tiếp'
+AND e.score = (
+    SELECT MAX(e2.score)
+    FROM enrollments e2
+    JOIN courses c2 ON e2.course_id = c2.id
+    WHERE c2.course_name = 'Tiếng Anh Giao Tiếp'
+);
+
+SELECT s.id,s.full_name
+FROM students s
+LEFT JOIN enrollments e ON s.id = e.student_id
+WHERE e.id IS NULL;
+
+DELETE FROM enrollments
+WHERE course_id = (
+    SELECT id FROM courses WHERE course_name = 'Triết học'
+);
+
+DELETE FROM courses
+WHERE course_name = 'Triết học';
